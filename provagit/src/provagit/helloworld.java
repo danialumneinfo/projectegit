5517219d392a8054a51c1b6e0f28e73c6ca2b1da
danialumneinfo <daniel.fernandez@insbaixcamp.cat>
2013-10-29 17:01:49 +0100

    Hola, hem fet un quart print

--- provagit/src/provagit/helloworld.java
+++ provagit/src/provagit/helloworld.java
@@ -7,6 +7,7 @@ public class helloworld {
 System.out.println("Hello world");
 System.out.println("Hello world 2");
 System.out.println("Hello world 3");
+System.out.println("Hello world 4");
 	}
 
 }
