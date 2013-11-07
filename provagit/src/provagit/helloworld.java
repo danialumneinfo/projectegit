3381b994177c0382d1f027e9df26dd792f774c65
danialumneinfo <daniel.fernandez@insbaixcamp.cat>
2013-11-07 01:23:22 +0100

    inclosa la prova de fetch

--- provagit/src/provagit/helloworld.java
+++ provagit/src/provagit/helloworld.java
@@ -8,6 +8,8 @@ System.out.println("Hello world");
 System.out.println("Hello world 2");
 System.out.println("Hello world 3");
 System.out.println("Hello world 4");
+
+System.out.println("prova de fetch 4");
 	}
 
 }
