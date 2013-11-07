2b8cad807ac7484889b8300c27020451a8648548
danialumneinfo <daniel.fernandez@insbaixcamp.cat>
2013-11-07 01:33:09 +0100

    Update helloworld.java
    prova fetch 3

--- provagit/src/provagit/helloworld.java
+++ provagit/src/provagit/helloworld.java
@@ -10,6 +10,8 @@ System.out.println("Hello world 3");
 System.out.println("Hello world 4");
 
 System.out.println("prova de fetch 4");
+System.out.println("Si ara ho modifico m'hauria de deixar fer fetch.");
+System.out.println("Si ara ho modifico m'hauria de deixar fer fetch 2.");
 	}
 
 }
